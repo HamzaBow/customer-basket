2c70c35ce81ee88f9a9af6d4f524db5eed200ee1
Hamza Bow <hamzabow77@gmail.com>
2022-06-03 23:56:47 +0100

    feat: increament & decrement product quantity in cart

--- src/CartSlice.ts
+++ src/CartSlice.ts
@@ -30,24 +30,52 @@ export const cartSlice = createSlice({
     addToCart: (state, { payload: productItem }: PayloadWrapper): void => {
       const cartItems = state.items.filter((cartItem: CartItem) => cartItem.productName === productItem.name)
       if (cartItems.length > 1) {
-        throw new Error("Two products of the same category cannot exist in Cart")
+        throw new Error("Two products of the same category cannot exist in Cart!")
+      }
+      if (cartItems.length === 1) {
+        throw new Error("Product already in cart!")
       }
       if (cartItems.length === 0) {
         state.items.push({ productName: productItem.name, quantity: 1 })
-        return
       }
-
-      // product.length === 1
-      // increment quantity of already added product to cart.
+    },
+    incrementQuantity: (state, {payload: name}): void => {
+      const cartItems = state.items.filter((cartItem: CartItem) => cartItem.productName === name)
+      if (cartItems.length === 0) {
+        state.items.push({ productName: name, quantity: 1 })
+        return;
+      }
       for (let i = 0; i < state.items.length; i++) {
-        if (state.items[i].productName === productItem.name){
+        if (state.items[i].productName === name){
           state.items[i].quantity += 1;
+          return;
         }
+      }
+    },
+    decrementQuantity: (state, {payload: name}): void => {
+      const cartItems = state.items.filter((cartItem: CartItem) => cartItem.productName === name)
+      if (cartItems.length === 1) {
+        if (cartItems[0].quantity === 1){  // remove item from cart
+          state.items = state.items.filter((cartItem: CartItem) => cartItem.productName !== name)
+          return;
+        }
+        for (let i = 0; i < state.items.length; i++) {
+          if (state.items[i].productName === name){
+            state.items[i].quantity -= 1;
+            return;
+          }
+        }
+      }
+      if (cartItems.length > 1) {
+        throw new Error("Two products of the same category cannot exist in Cart!")
+      }
+      if (cartItems.length === 0) {
+        throw new Error("Product not in cart!")
       }
     }
   }
 })
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, incrementQuantity, decrementQuantity } = cartSlice.actions;
 
 export default cartSlice.reducer;
--- src/components/CartItemView.tsx
+++ src/components/CartItemView.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
-import { CartItem } from '../CartSlice';
+import { useDispatch } from 'react-redux';
+import { CartItem, decrementQuantity, incrementQuantity } from '../CartSlice';
 interface Props {
   cartItem: CartItem
 }
 const CartItemView:React.FC<Props> = ({ cartItem }) => {
+  const dispatch = useDispatch();
+  const incrementQ = () => {
+    dispatch(incrementQuantity(cartItem.productName))
+  }
+  const decrementQ = () => {
+    dispatch(decrementQuantity(cartItem.productName))
+  }
   return (
     <>
       <h3>{cartItem.productName}</h3>
       <h3>{cartItem.quantity}</h3>
+      <button onClick={incrementQ}>+</button>
+      <button onClick={decrementQ}>-</button>
     </>
   )
 }

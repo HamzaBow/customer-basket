e91c546e7f9996a3a6ab42623211a9900a4623ad
Hamza Bow <hamzabow77@gmail.com>
2022-06-03 17:54:31 +0100

    add basic empty components to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import Cart from "./components/Cart";
+import Products from "./components/Products";
+
 function App() {
   return (
     <div className="App">
+      <Products />
+      <Cart />
     </div>
   );
 }

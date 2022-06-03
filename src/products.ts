25825011e806113950c0bb0e464a15eb0613184b
Hamza Bow <hamzabow77@gmail.com>
2022-06-03 17:45:49 +0100

    add a list of products to choose from.

---
+++ src/products.ts
@@ -0,0 +1,21 @@
+interface Product {
+  name: string;
+  cost: number;
+}
+
+const products: Product[] = [
+  {
+    name: "Butter",
+    cost: 0.8
+  },
+  {
+    name: "Milk",
+    cost: 1.15
+  },
+  {
+    name: "Bread",
+    cost: 1
+  }
+]
+
+export default products;

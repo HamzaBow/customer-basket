8db0ef2146dead992a18d782139b5b66ffa42111
Hamza Bow <hamzabow77@gmail.com>
2022-06-04 22:05:41 +0100

    fix: move fontawesome link from component to index.html

--- src/components/StarRating.tsx
+++ src/components/StarRating.tsx
@@ -6,10 +6,6 @@ const StarRating = () => {
   }
   return (
     <div>
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-      />
       <span style={checkedStyle} className="fa fa-star"></span>
       <span style={checkedStyle} className="fa fa-star"></span>
       <span style={checkedStyle} className="fa fa-star"></span>

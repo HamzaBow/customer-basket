1c7ff9870cfe961aa281c793348fe3fd428eca11
Hamza Bow <hamzabow77@gmail.com>
2022-06-04 20:24:53 +0100

    refactor: move cartItem style to css file

--- src/components/CartItemView.tsx
+++ src/components/CartItemView.tsx
@@ -2,6 +2,8 @@ import React, { CSSProperties, useEffect } from 'react'
 import { useDispatch } from 'react-redux';
 import { CartItem, decrementQuantity, incrementQuantity, applyDiscounts } from '../CartSlice';
 import { moneyFormat } from '../utils';
+import "../App.css";
+
 interface Props {
   cartItem: CartItem
 }
@@ -32,30 +34,19 @@ const CartItemView:React.FC<Props> = ({ cartItem }) => {
     dispatch(applyDiscounts());
   }, [cartItem.quantity])
 
-  const itemOldTotalStyle: CSSProperties = {
-    float: "right",
-    textDecoration: "line-through",
-    textDecorationThickness: "1.5px",
-    textDecorationColor: "red",
-    color: "red"
-  }
-
-  const itemNewTotalStyle: CSSProperties = {
-    float: "right",
-  };
 
   return (
     <div style={cartItemStyle}>
       <h3>{cartItem.productName}</h3>
       {!!cartItem.discount && (
         <>
-          <h3 style={itemOldTotalStyle}>
+          <h3 className="item-old-total">
             {moneyFormat(cartItem.unitCost * cartItem.quantity)}
           </h3>
           <br />
         </>
       )}
-      <h3 style={itemNewTotalStyle}>
+      <h3 className='item-new-total'>
         {moneyFormat(cartItem.unitCost * cartItem.quantity - cartItem.discount)}
       </h3>
       <h3 style={{ display: "inline-block", marginRight: "15px" }}>Quantity</h3>

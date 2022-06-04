3329ede856a235f75146e64f2e3d5d0453cfe9df
Hamza Bow <hamzabow77@gmail.com>
2022-06-04 12:56:04 +0100

    refactor: move 'add to cart' btn style to a css file

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -3,6 +3,7 @@ import { addToCart } from "../CartSlice"
 import { useDispatch } from "react-redux";
 import { ProductItem } from "../productData"
 import { moneyFormat } from "../utils";
+import "../App.css"
 
 interface Props {
   productItem: ProductItem;
@@ -18,18 +19,11 @@ const Product:React.FC<Props> = ({ productItem }) => {
   const handleAddToCart = () => {
     dispatch(addToCart(productItem));
   };
-  const btnAddStyle: CSSProperties = {
-    fontSize: "16pt",
-    padding: "5px 15px",
-    backgroundColor: "yellow",
-    color: "black",
-    borderRadius: "5px",
-  }
   return (
     <div style={productStyle}>
       <h3>{productItem.name}</h3>
       <h3>{moneyFormat(productItem.cost)}</h3>
-      <button onClick={handleAddToCart} style={btnAddStyle}>Add to cart</button>
+      <button onClick={handleAddToCart} className={"btn-add-to-cart"}>Add to cart</button>
     </div>
   );
 }
